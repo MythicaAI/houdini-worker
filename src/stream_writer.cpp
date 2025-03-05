6b4e4b49a57a2c159f7c0d88c6a0833be19f8f54
Kevin Calderone <kevincalderone@gmail.com>
2025-03-05 09:24:40 -0800

    Simplify worker logging to reduce overhead (#24)
    Cleanup logging
    
    Co-authored-by: kevin <kevin@mythica.ai>

--- src/stream_writer.cpp
+++ src/stream_writer.cpp
@@ -76,7 +76,6 @@ void StreamWriter::geometry(const Geometry& geometry)
 
 void StreamWriter::writeToStream(const std::string& op, const std::string& data)
 {
-    util::log() << "Sending response: " << op << " " << (data.length() > 100 ? data.substr(0, 97) + "..." : data) << std::endl;
     std::string json = "{\"op\":\"" + op + "\",\"data\":" + data + "}\n";
     m_websocket.push_response(StreamMessage{m_connection_id, json});
 }
